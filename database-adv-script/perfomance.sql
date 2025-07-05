faca1b02d394d9d0736ad185a99700702b425e90
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-05 10:43:05 +0100

    added performance .sql file

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -21,23 +21,21 @@ JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
 
 -- Initial unoptimized query
-SELECT
+EXPLAIN 
+SELECT 
     b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.status,
-    u.user_id,
-    u.first_name,
-    u.last_name,
+    u.name AS user_name,
     u.email,
-    p.property_id,
     p.name AS property_name,
     p.location,
-    pay.payment_id,
     pay.amount,
-    pay.payment_date,
-    pay.payment_method
+    pay.payment_method,
+    b.status,
+    b.start_date,
+    b.end_date
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+AND p.location = 'Lisbon';
