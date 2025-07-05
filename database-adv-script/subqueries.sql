c18393b97022ee946678f4ceab144df77ce5630d
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-05 09:07:29 +0100

    Added a subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,36 @@
+-- 1. Non-Correlated Subquery: Properties with average rating > 4.0
+SELECT 
+    property_id,
+    name,
+    location,
+    pricepernight
+FROM 
+    properties
+WHERE 
+    property_id IN (
+        SELECT 
+            property_id
+        FROM 
+            reviews
+        GROUP BY 
+            property_id
+        HAVING 
+            AVG(rating) > 4.0
+    );
+
+-- 2. Correlated Subquery: Users who have made more than 3 bookings
+SELECT 
+    user_id,
+    name,
+    email
+FROM 
+    users u
+WHERE 
+    (
+        SELECT 
+            COUNT(*) 
+        FROM 
+            bookings b 
+        WHERE 
+            b.user_id = u.user_id
+    ) > 3;
